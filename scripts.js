6dbc3587269166eeafd44c9f37bc50d3bd2d0d66
homeroteodoro <57195048+homeroteodoro@users.noreply.github.com>
2019-12-11 23:19:35 +0000

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,17 @@
+const modalOverlay = document.querySelector('.modal-overlay')
+const cards = document.querySelectorAll('.card')
+
+for (let card of cards) {
+    card.addEventListener("click", function() {
+        let modalImg = card.getAttribute("id");
+        modalOverlay.classList.add('active')
+        modalOverlay.querySelector("img").src = `/foodfy/assets/${modalImg}.png`;
+        modalOverlay.querySelector('h4').innerHTML = card.querySelector('h4').textContent
+        modalOverlay.querySelector('p').innerHTML = card.querySelector('p').textContent
+    })
+}
+
+document.querySelector(".close-modal").addEventListener("click", function() {
+    modalOverlay.classList.remove("active")
+    modalOverlay.querySelector("").src = ""
+})
